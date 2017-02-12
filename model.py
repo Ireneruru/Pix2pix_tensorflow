d0591992ecadd5115897ed99ca06b06a98340cd0
Yiru Chen <chen1ru@outlook.com>
2017-02-12 21:40:53 +0800

    add

--- model.py
+++ model.py
@@ -1,33 +1,54 @@
 import tensorflow as tf
 import numpy as np
+from glob import glob
+from Preprocess import *
 
-# Create 100 phony x, y data points in NumPy, y = x * 0.1 + 0.3
-x_data = np.random.rand(100).astype(np.float32)
-y_data = x_data * 0.1 + 0.3
+class CGAN(object):
+    def __init__(self, sess, image_size=240,
+                 batch_size=1, sample_size=1, output_size=256,
+                 gf_dim=64, df_dim=64, L1_lambda=100,
+                 input_c_dim=3, output_c_dim=3, dataset_name='facades',
+                 checkpoint_dir=None, sample_dir=None):
 
-# Try to find values for W and b that compute y_data = W * x_data + b
-# (We know that W should be 0.1 and b 0.3, but TensorFlow will
-# figure that out for us.)
-W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-b = tf.Variable(tf.zeros([1]))
-y = W * x_data + b
+        """
+        Args:
+            sess: TensorFlow session
+            batch_size: The size of batch. Should be specified before training.
+            output_size: (optional) The resolution in pixels of the images. [256]
+            gf_dim: (optional) Dimension of gen filters in first conv layer. [64]
+            df_dim: (optional) Dimension of discrim filters in first conv layer. [64]
+            input_c_dim: (optional) Dimension of input image color. For grayscale input, set to 1. [3]
+            output_c_dim: (optional) Dimension of output image color. For grayscale input, set to 1. [3]
+        """
+        #input
+        self.real_img = tf.placeholder(tf.float32, [self.batch_size, self.image_size, self.image_size,
+                                                    self.input_c_dim + self.output_c_dim], name='real_A_and_B_images')
+        self.condition = tf.placeholder()
 
-# Minimize the mean squared errors.
-loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
+        #random seed
+        self.noise = tf.random_normal()
 
-# Before starting, initialize the variables.  We will 'run' this first.
-init = tf.global_variables_initializer()
 
-# Launch the graph.
-sess = tf.Session()
-sess.run(init)
+        #discriminator
+        
 
-# Fit the line.
-for step in range(201):
-    sess.run(train)
-    if step % 20 == 0:
-        print(step, sess.run(W), sess.run(b))
 
-# Learns best fit is W: [0.1], b: [0.3]
+
+        #generator
+
+
+
+
+
+
+
+    def load_random_samples(self):
+        data = np.random.choice(glob('./datasets/{}/val/*.jpg'.format(self.dataset_name)), self.batch_size)
+        sample = [load_data(sample_file) for sample_file in data]
+
+        if (self.is_grayscale):
+            sample_images = np.array(sample).astype(np.float32)[:, :, :, None]
+        else:
+            sample_images = np.array(sample).astype(np.float32)
+        return sample_images
+
