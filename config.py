2b121d0f178909cc5270c4359e759873c44d3939
Yiru Chen <chen1ru@outlook.com>
2017-02-13 17:09:35 +0800

    initial versin

--- config.py
+++ config.py
@@ -2,6 +2,11 @@
 
 class Config:
     data_path = "./datasets/facades"
+    img_size = 256
+    img_channel = 3
+    conv_channel_base = 64
+
     learning_rate = 0.01
     max_epoch = 100
-    L1_lambda
+    L1_lambda = 0.1
+
